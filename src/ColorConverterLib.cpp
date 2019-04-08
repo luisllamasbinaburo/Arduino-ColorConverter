9ce7482f5a37838818ac606a55534bf71b488fa0
Luis Llamas <luisllamas@hotmail.com>
2019-04-08 17:02:54 +0200

    Update ColorConverterLib.cpp

--- src/ColorConverterLib.cpp
+++ src/ColorConverterLib.cpp
@@ -7,7 +7,7 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License
  ****************************************************/
  
-#include "ColorConverter.h"
+#include "ColorConverterLib.h"
 
 void ColorConverter::RgbToHsv(uint8_t red, uint8_t green, uint8_t blue, double& hue, double& saturation, double& value)
 {
